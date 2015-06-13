5c7d6ae85b1777821c64181b43d61c10d49925f6
Kimerlee Holcom <kimerlee.holcom@gmail.com>
2015-06-12 21:52:31 -0400

    finished week 2

--- Holcom_Kimerlee_ProjectWk2/js/main.js
+++ Holcom_Kimerlee_ProjectWk2/js/main.js
@@ -7,18 +7,23 @@ $(document).ready(function(){
 	nav.on('mouseenter', function(){
 		if($(this).text() === "Forums"){
 			$('#forum').slideDown();
-		}else if($(this).text() != "Forums"){
+		}else if(($(this).text() != "Forums")){
 			$('#forum').slideUp();
 		}
 	});
 
+	$('section').on('mouseenter', function(){
+		$('#forum').slideUp();
+	});
 
 	/**************** CLICK FUNCTIONS ****************/
 	nav.on('click', function(){
+		//Move Header up to top from middle
 		$('header').animate({'top': '0'}, 'slow');
 		$('section').fadeIn('slow');
 		var link = ($(this).text().toLowerCase());
 
+		/**************** TABBED/ACCORDIAN PANEL ****************/
 		if(link === 'home'){
 			$('#forums, #gallery').css('display', 'none');
 			$('#home').fadeIn('slow');
@@ -37,5 +42,35 @@ $(document).ready(function(){
 		$('#home, #gallery').css('display', 'none');
 		
 	});
-	
+
+	/**************** MODAL ****************/
+	$('.modClick').click(function(e){
+		//Open Modal
+		e.preventDefault();
+		$('#modBG').fadeIn();
+	});
+
+	$('#close').click(function(e){
+		//Close Modal
+		e.preventDefault();
+		$('#modBG').fadeOut();
+	});
+
+	/**************** TOOLTIP ****************/
+	$('.tool').hover(function(){
+		//Display ToolTip
+		var title = $(this).attr('title');
+		$(this).data('tipText', title).removeAttr('title');
+		$('<p class="tooltip"></p>').text(title).appendTo('body').fadeIn();
+
+	}, function(){
+		//Remove ToolTip
+		$(this).attr('title', $(this).data('tipText'));
+		$('.tooltip').remove();
+	}).mousemove(function(e){
+		//Place Tooltip at mouse point
+		var mX = e.pageX;
+		var mY = e.pageY;
+		$('.tooltip').css({'top' : mY, 'left': mX});
+	});
 });
